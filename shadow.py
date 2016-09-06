b02e6ada66ebd1f490137c08bbb8ce21f17a37b8
Sean-PC\Sean <seanreid.mail@gmail.com>
2016-09-06 13:55:56 -0700

    Fixed filter nomenclature- c3_mean named twice

--- shadow.py
+++ shadow.py
@@ -39,21 +39,21 @@ def seedDetect(c3_b,S,V,k,Tv,Ts):
 #    Ts = 0.02
     c3_mean = np.mean(c3_b)
     kernel = np.ones((k,k))
-    c3_mean = ndi.generic_filter(c3_b,f,footprint=kernel,mode='constant',cval=0)
+    c3_box = ndi.generic_filter(c3_b,f,footprint=kernel,mode='constant',cval=0)
     V_mean = ndi.generic_filter(V,f,footprint=kernel,mode='constant',cval=0)
     S_mean = ndi.generic_filter(S,f,footprint=kernel,mode='constant',cval=0)
-    c3_gt_mean = c3_mean > c3_mean
-    V_gt_mean = V_mean < Tv
-    S_gt_mean = S_mean > Ts
+    c3_gt_mean = c3_box > c3_mean
+    V_lt_thresh = V_mean < Tv
+    S_gt_thresh = S_mean > Ts
     coordinates = peak_local_max(c3_b, min_distance=k)
-    shadow_inds = np.empty((0,2))
+    seed_inds = np.empty((0,2))
     for c in coordinates:
         i = c[0]
         j = c[1]
         if c3_gt_mean[i,j]:
-            if V_gt_mean[i,j]:
-                if S_gt_mean[i,j]:
-                    seed_inds = np.append(shadow_inds, [[i,j]])
+            if V_lt_thresh[i,j]:
+                if S_gt_thresh[i,j]:
+                    seed_inds = np.append(seed_inds, [[i,j]],0)
                     
     return seed_inds
             
